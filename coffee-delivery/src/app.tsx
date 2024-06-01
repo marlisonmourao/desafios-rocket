86eef50ba153b7c16e0c76c5b467518cf8800e16
marlisonmourao <bentesmourao@gmail.com>
2024-06-01 00:32:57 -0400

    feat: initial commit

---
+++ coffee-delivery/src/app.tsx
@@ -0,0 +1,11 @@
+import { Header } from './components/header'
+
+export function App() {
+  return (
+    <div>
+      <div className="mt-6">
+        <Header />
+      </div>
+    </div>
+  )
+}
---
+++ coffee-delivery/tailwind.config.js
@@ -0,0 +1,15 @@
+import { colors } from './src/theme/colors'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    extend: {
+      colors,
+      fontFamily: {
+        body: 'Roboto, sans-serif',
+      },
+    },
+  },
+  plugins: [],
+}
